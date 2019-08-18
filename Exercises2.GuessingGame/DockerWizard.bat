8b6dbddfab9ba39c1e2db7f1d2208f81edb6dcde
Paolo Gabrielli <paolo.gabrielli.85@gmail.com>
2019-08-18 18:11:15 +0200

    Aggiungere i file di progetto.

---
+++ Exercises2.GuessingGame/DockerWizard.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+
+echo Publish project...
+dotnet publish Exercises2.GuessingGame.csproj -o bin/Publish -c Release
+
+echo Build image
+docker build -t guessing-game:1.0 .
+
+echo Tag lastest image
+docker tag guessing-game:1.0 guessing-game:latest
+
+echo Completed
---
+++ Exercises2.GuessingGame/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Exercises2.GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+
+            while (true)
+            {
+                int attempt = 0;
+                Console.Clear();
+                Console.WriteLine("Ho generato un numero... prova ad indovinare!");
+                int number = random.Next(1, 100);
+
+                while (true)
+                {
+                    Write("Indovina il mio numero (1 a 100): ");
+                    string input = Console.ReadLine();
+
+                    int value = 0;
+                    bool isnumber = int.TryParse(input, out value);
+
+                    if (!isnumber)
+                    {
+                        Write("Non hai immesso un numero valido!", ConsoleColor.Red);
+                        continue;
+                    }
+
+                    if (value < 1 || value > 100)
+                    {
+                        Write("Il numero deve essere nell'intervallo 1-100!", ConsoleColor.Red);
+                        continue;
+                    }
+
+                    var result = Math.Sign(number - value);
+                    attempt++;
+
+                    if (result == 0)
+                    {
+                        Write($"Bravo hai indovinato in {attempt} tentativi!!!", ConsoleColor.Green);
+                        break;
+                    }
+                    else if (result < 0)
+                    {
+                        Write("Il numero è minore");
+                        continue;
+                    }
+                    else if (result > 0)
+                    {
+                        Write("Il numero è maggiore");
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private static void Write(string message, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
